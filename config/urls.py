0fd7efe728306b1822941311f63c7049f4e1ffcd
Kutay Ozgun <kkutayozgun@gmail.com>
2022-07-23 17:21:21 +0300

    added robots.txt

--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib.sitemaps.views import sitemap
 from django.utils.translation import ugettext_lazy as _
+from django.views.generic import TemplateView
 
 from common.sitemaps import CommonStaticViewSitemap
 from config.views import GenderSelectionPage
@@ -32,6 +33,7 @@ urlpatterns = [
     path(settings.ADMIN_URL, admin.site.urls),
     path('i18n/', include('django.conf.urls.i18n')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}),
+    path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
     path('', include('common.urls')),
 ]
 
