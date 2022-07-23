11674a3615dc0aef087ef1f80385576d307ebebe
Kutay Ozgun <kkutayozgun@gmail.com>
2022-07-23 17:23:21 +0300

    added robots.txt

--- config/urls.py
+++ config/urls.py
@@ -33,7 +33,6 @@ urlpatterns = [
     path(settings.ADMIN_URL, admin.site.urls),
     path('i18n/', include('django.conf.urls.i18n')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}),
-    path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
     path('', include('common.urls')),
 ]
 
